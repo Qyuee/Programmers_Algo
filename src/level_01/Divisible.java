ea135d9acf8e51f0b2d6a84b4149c8fa45ae0a70
bit <bit@DESKTOP-AMM3Q1U>
2018-02-26 09:14:00 +0900

    test

---
+++ src/level_01/Divisible.java
@@ -0,0 +1,31 @@
+package level_01;
+import java.util.Arrays;
+
+class Divisible {
+	public int[] divisible(int[] array, int divisor) {
+		//ret에 array에 포함된 정수중, divisor로 나누어 떨어지는 숫자를 순서대로 넣으세요.
+		int count=0;
+		for(int i=0; i<array.length; i++) {
+			if((array[i]%divisor)==0) {
+				count++;
+			}
+		}
+		
+		int inner_count=0;
+		int[] ret = new int[count];
+		for(int i=0; i<array.length; i++) {
+			if((array[i]%divisor)==0) {
+				ret[inner_count]=array[i];
+				inner_count++;
+			}
+		}
+		
+		return ret;
+	}
+	// 아래는 테스트로 출력해 보기 위한 코드입니다.
+	public static void main(String[] args) {
+		Divisible div = new Divisible();
+		int[] array = {5, 9, 7, 10};
+		System.out.println( Arrays.toString( div.divisible(array, 5) ));
+	}
+}
